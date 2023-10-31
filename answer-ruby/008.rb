aaff184cd3893dff589dec41558f7677517f127f
tenkoh <tenkoh.go@gmail.com>
2023-10-31 09:28:07 +0000

    008。正規表現のキャプチャを使い必要な部分だけ抽出。

---
+++ answer-ruby/008.rb
@@ -0,0 +1,13 @@
+logs = File.open('qdata/8/access.log', 'r').readlines
+
+# am? receives a record including 02/Jun/2016:15:04:05 +0900 time format
+def am?(record)
+  re = %r{\[\d{2}/[a-zA-Z]{3}/\d{4}:(\d{2}):}
+  h = record.match(re)[1].to_i
+  puts h
+  h < 12
+end
+
+n_am = logs.count { |log| am?(log) }
+n_pm = logs.size - n_am
+puts "午前: #{n_am}\n午後: #{n_pm}"
